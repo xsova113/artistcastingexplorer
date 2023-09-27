04801920d00ea2d7f2bf888174621b1a3d538ba5
George <xsova113@gmail.com>
2023-09-26 22:34:45 -0700

    bug fix

--- app/(main)/(routes)/directory/components/TalentCard.tsx
+++ app/(main)/(routes)/directory/components/TalentCard.tsx
@@ -73,7 +73,7 @@ const TalentCard = ({
 
     setFavouritedTalent(response);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [onSave]);
+  }, [loading]);
 
   useEffect(() => {
     setIsMounted(true);
