cdd390771753fd94c1e91078e93128a00c26d327
George <xsova113@gmail.com>
2023-11-06 15:51:06 -0800

    bug fix attempt

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,24 +1,24 @@
-import { Post } from "@/types/post";
-import axios from "axios";
-import { MetadataRoute } from "next";
+// import { Post } from "@/types/post";
+// import axios from "axios";
+// import { MetadataRoute } from "next";
 
-const URL = "https://www.artistcastingexplorer.com";
+// const URL = "https://www.artistcastingexplorer.com";
 
-const news = async (): Promise<Post[]> => {
-  const posts = (
-    await axios.get("https://castingjapanese.ca/wp-json/wp/v2/posts")
-  ).data;
+// const news = async (): Promise<Post[]> => {
+//   const posts = (
+//     await axios.get("https://castingjapanese.ca/wp-json/wp/v2/posts")
+//   ).data;
 
-  return posts;
-};
+//   return posts;
+// };
 
-export default async function sitemap() {
-  const posts = await news().then((data) =>
-    data.map(({ id, date, categories }) => ({
-      url: `${URL}/${categories[0] === 1418 ? "news" : "interviews"}/${id}`,
-      lastModified: date,
-    })),
-  );
+// export default async function sitemap() {
+//   const posts = await news().then((data) =>
+//     data.map(({ id, date, categories }) => ({
+//       url: `${URL}/${categories[0] === 1418 ? "news" : "interviews"}/${id}`,
+//       lastModified: date,
+//     })),
+//   );
 
-  return [...posts];
-}
+//   return [...posts];
+// }
