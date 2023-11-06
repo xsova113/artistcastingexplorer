b94e4545d3170af376280dd4ff62c9b0a49739e7
George <xsova113@gmail.com>
2023-11-06 13:13:00 -0800

    add id in script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,11 +57,12 @@ export default function RootLayout({
         </head>
 
         <Script
+          id=""
           strategy="lazyOnload"
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
 
-        <Script strategy="lazyOnload">
+        <Script id="" strategy="lazyOnload">
           {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
