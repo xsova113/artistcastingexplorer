287b251d638d95351ae23faf1abe7f3a0372bd3e
George <xsova113@gmail.com>
2023-11-08 16:56:43 -0800

    minor update

--- app/(blogs)/news1/page.tsx
+++ app/(blogs)/news1/page.tsx
@@ -3,6 +3,8 @@ import { BlogPost } from "@/types/post";
 import ArticleCard from "../_components/ArticleCard";
 import Archive from "../_components/Archive";
 
+export const revalidate = 0;
+
 const BlogPage = async () => {
   const posts: BlogPost[] = await client.fetch(
     `*[_type == 'post' && categories[] -> title match "news"]`,
@@ -10,7 +12,7 @@ const BlogPage = async () => {
 
   return (
     <section className="max-w-screen-lg px-2.5 py-20 md:px-10">
-      <div className="flex flex-col gap-y-4 mb-12">
+      <div className="mb-12 flex flex-col gap-y-4">
         <h1 className="text-4xl font-semibold md:text-5xl">
           News and Upcoming Events
         </h1>
@@ -18,7 +20,7 @@ const BlogPage = async () => {
           Explore here for details on artist appearances and more
         </h3>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
         {posts.map((post) => (
           // <div key={post._id}>
           //   <Link href={`/news1/${post.slug.current}`}>{post.slug.current}</Link>
