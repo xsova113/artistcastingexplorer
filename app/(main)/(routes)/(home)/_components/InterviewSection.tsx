90cac21f9bee9211ab7c643611dfc59418690d3c
George <xsova113@gmail.com>
2023-11-16 14:16:17 -0800

    update suspense loading for blog section

--- app/(main)/(routes)/(home)/_components/InterviewSection.tsx
+++ app/(main)/(routes)/(home)/_components/InterviewSection.tsx
@@ -1,9 +1,11 @@
 import ArticleCard from "@/app/(blogs)/_components/ArticleCard";
+import Loading from "@/app/loading";
 import Stack from "@/components/Stack";
 import { buttonVariants } from "@/components/ui/button";
 import { client } from "@/sanity/lib/client";
 import { BlogPost } from "@/types/post";
 import Link from "next/link";
+import { Suspense } from "react";
 
 const InterviewSection = async () => {
   const posts: BlogPost[] = await client.fetch(
@@ -21,15 +23,18 @@ const InterviewSection = async () => {
         <p className="text-center">Discover the latest stories here</p>
       </div>
 
-      {!posts || posts.length === 0 ? (
-        <p>Cannot load...</p>
-      ) : (
-        <div className="grid grid-cols-1 justify-center gap-4 lg:grid-cols-3">
-          {posts.slice(0, 3).map((post: BlogPost) => (
-            <ArticleCard key={post._id} path="interviews" post={post} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Loading />}>
+        {!posts || posts.length === 0 ? (
+          <p>Cannot load...</p>
+        ) : (
+          <div className="grid grid-cols-1 justify-center gap-4 lg:grid-cols-3">
+            {posts.slice(0, 3).map((post: BlogPost) => (
+              <ArticleCard key={post._id} path="interviews" post={post} />
+            ))}
+          </div>
+        )}
+      </Suspense>
+
       <Link
         href="/interviews"
         className={buttonVariants({
--- app/(main)/(routes)/(home)/_components/NewSection.tsx
+++ app/(main)/(routes)/(home)/_components/NewSection.tsx
@@ -1,10 +1,12 @@
 // import ArticleCard from "@/components/ArticleCard";
 import ArticleCard from "@/app/(blogs)/_components/ArticleCard";
+import Loading from "@/app/loading";
 import Stack from "@/components/Stack";
 import { buttonVariants } from "@/components/ui/button";
 import { client } from "@/sanity/lib/client";
 import { BlogPost } from "@/types/post";
 import Link from "next/link";
+import { Suspense } from "react";
 
 const NewSection = async () => {
   const posts: BlogPost[] = await client.fetch(
@@ -22,15 +24,18 @@ const NewSection = async () => {
         </p>
       </div>
 
-      {!posts || posts.length === 0 ? (
-        <p>Cannot load...</p>
-      ) : (
-        <div className="grid grid-cols-1 justify-center gap-4 lg:grid-cols-3">
-          {posts.slice(0, 3).map((post: BlogPost) => (
-            <ArticleCard key={post._id} path={"news"} post={post} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Loading />}>
+        {!posts || posts.length === 0 ? (
+          <p>Cannot load...</p>
+        ) : (
+          <div className="grid grid-cols-1 justify-center gap-4 lg:grid-cols-3">
+            {posts.slice(0, 3).map((post: BlogPost) => (
+              <ArticleCard key={post._id} path={"news"} post={post} />
+            ))}
+          </div>
+        )}
+      </Suspense>
+
       <Link
         href="/news"
         className={buttonVariants({
