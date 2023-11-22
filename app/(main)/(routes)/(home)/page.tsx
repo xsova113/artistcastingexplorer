64d91de8d318810f921a3edcb64afc65e324aaf6
George <xsova113@gmail.com>
2023-11-22 00:00:42 -0800

    minor update

--- app/(main)/(routes)/(home)/page.tsx
+++ app/(main)/(routes)/(home)/page.tsx
@@ -2,17 +2,14 @@ import HeroSection from "./_components/HeroSection";
 import ServiceSection from "./_components/FeatureSection";
 import { getTalents } from "@/actions/getTalents";
 import dynamic from "next/dynamic";
-import Loading from "@/app/loading";
 
-const DiscoverSection = dynamic(() => import("./_components/DiscoverSection"), {
-  loading: () => <Loading />,
-});
-const NewSection = dynamic(() => import("./_components/NewSection"), {
-  loading: () => <Loading />,
-});
+const DiscoverSection = dynamic(
+  () => import("./_components/DiscoverSection"),
+  {},
+);
+const NewSection = dynamic(() => import("./_components/NewSection"), {});
 const InterviewSection = dynamic(
   () => import("./_components/InterviewSection"),
-  { loading: () => <Loading /> },
 );
 
 const HomePage = async () => {
