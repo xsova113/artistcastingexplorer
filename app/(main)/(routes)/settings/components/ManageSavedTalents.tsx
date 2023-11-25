7314c34af1e0c5185e97c4394d822f9abdbc6995
George <xsova113@gmail.com>
2023-11-24 21:11:21 -0800

    bug fix attempt

--- app/(main)/(routes)/settings/components/ManageSavedTalents.tsx
+++ app/(main)/(routes)/settings/components/ManageSavedTalents.tsx
@@ -1,30 +1,40 @@
+"use client";
+
 import Stack from "@/components/Stack";
 import { DataTable } from "./DataTable";
 import { columns } from "./Columns";
 import prisma from "@/lib/client";
 import { auth } from "@clerk/nextjs";
+import { useQuery, useQueryClient } from "react-query";
 
-const ManageSavedTalents = async () => {
+const ManageSavedTalents = () => {
   const { userId } = auth();
+  const queryClient = useQueryClient();
 
-  const savedTalents = !userId
-    ? []
-    : await prisma.talentProfile.findMany({
+  const { data: savedTalents } = useQuery({
+    queryFn: async () =>
+      await prisma.talentProfile.findMany({
         where: { likes: { has: userId } },
         include: {
           images: true,
           performerType: true,
           location: true,
         },
-      });
+      }),
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ["savedTalents"] }),
+  });
 
-  const formattedData = savedTalents.map((talent) => ({
-    id: talent.id,
-    name: talent.firstName + " " + talent.lastName,
-    image: talent.images.map((img) => img.url)[0],
-    role: talent.performerType.role,
-    location: talent.location.city || talent.location.province,
-  }));
+  const formattedData =
+    !userId || !savedTalents
+      ? []
+      : savedTalents.map((talent) => ({
+          id: talent.id,
+          name: talent.firstName + " " + talent.lastName,
+          image: talent.images.map((img) => img.url)[0],
+          role: talent.performerType.role,
+          location: talent.location.city || talent.location.province,
+        }));
 
   return (
     <Stack>
