3bdc7c644431846c97fcde6465e73eb0b7d1b3ae
George <xsova113@gmail.com>
2023-12-03 15:13:24 -0800

    minor update

--- app/(blogs)/layout.tsx
+++ app/(blogs)/layout.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 0;
+
 const BlogsLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="mx-auto max-w-screen-lg px-2.5 py-20 md:px-10">
