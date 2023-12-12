3d96c63e7a6083a85ea15e7368bd499bed9fa4b8
George <xsova113@gmail.com>
2023-12-11 20:17:27 -0800

    minor update

--- app/(main)/(routes)/profile/_components/TalentBio.tsx
+++ app/(main)/(routes)/profile/_components/TalentBio.tsx
@@ -2,7 +2,7 @@
 
 import Stack from "@/components/Stack";
 import { Separator } from "@/components/ui/separator";
-import { cmToInFt, cn } from "@/lib/utils";
+import { cmToInFt } from "@/lib/utils";
 import { TalentProfileType } from "@/types/talentProfileType";
 import dynamic from "next/dynamic";
 import { Raleway } from "next/font/google";
@@ -88,7 +88,15 @@ const TalentBio = ({ talent }: TalentBio) => {
 
       <Stack className="gap-4">
         <h3 className="text-lg font-medium">About me</h3>
-        <ReactQuill theme="bubble" value={talent.bio || "No bio..."} readOnly />
+        {!talent.bio?.includes("<p>") ? (
+          <pre>{talent.bio}</pre>
+        ) : (
+          <ReactQuill
+            theme="bubble"
+            value={talent.bio || "No bio..."}
+            readOnly
+          />
+        )}
       </Stack>
 
       <Separator className="my-5" />
